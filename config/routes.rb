17de2f79688f6f23ae7c5b8aac3141350d2d2ce3
Nick Goddard <nicko777@gmail.com>
2010-10-17 00:59:26 +0100

    first pass of clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :clients
+
+  map.resources :companies
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
